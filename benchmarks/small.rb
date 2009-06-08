d5b6ab192054498e92fbf841b3ed19b4ad2714e1
visionmedia <tj@vision-media.ca>
2009-06-08 10:12:13 -0700

    Added examples

--- benchmarks/small.rb
+++ benchmarks/small.rb
@@ -24,4 +24,25 @@ benchmark '', :times => 200 do
     select :days, { :mon => 'Monday', :fri => 'Friday' }, :id => 'days', 
       :label => 'Days', :required => true, :description => 'Select a day.'
   end
+  report 'full form' do 
+    form :register, :action => '/register' do
+      fieldset :login, 'Login Information' do
+        text :username, :label => 'Username'
+        password :password, :label => 'Password'
+        password :password_confirm, :required => true, :description => 'Enter password between 6 - 30 characters.'
+      end
+      fieldset :account, 'Account Details' do
+        text :country, :label => 'Country'
+        text :city, :label => 'City'
+        fieldset :forum, 'Forum Related' do
+          file :image, :label => 'Avatar'
+          textarea :signature, :label => 'Signature'
+        end
+      end
+      buttons do
+        submit :op, 'Join'
+        submit :cancel, 'Cancel'
+      end
+    end
+  end
 end
