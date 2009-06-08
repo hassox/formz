dfca747917bc9db221dee56dd52a9c7b17c14811
visionmedia <tj@vision-media.ca>
2009-06-08 09:08:06 -0700

    Added wrappers specs

---
+++ spec/wrappers_spec.rb
@@ -0,0 +1,18 @@
+
+describe Formz do
+  describe "wrappers" do
+    before :each do
+      @markup = tag :input, :type => :file, :name => :upload
+    end
+    
+    it "should wrap a field in a div for styling" do
+      @markup.should have_tag('div') do |div|
+        div.should have_tag('input')
+      end
+    end
+    
+    it "should add classes for the type of field and its name" do
+      @markup.should have_tag('div.field-upload.field-file')
+    end
+  end
+end
