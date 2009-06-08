8e76cbc1720815ac393cced653f3ea1baefbbe14
visionmedia <tj@vision-media.ca>
2009-06-08 09:25:23 -0700

    Nested select optgroup specs

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -9,6 +9,24 @@ describe Formz do
           select.should have_tag('option[@value=bc]', 'British Columbia')
         end
       end
+      
+      it "should create nested optgroups" do
+        options = {}
+        options['Canada'] = { :ab => 'Alberta', :bc => 'British Columbia' }
+        options['United States'] = { :oh => 'Ohio' }
+        options[:other] = 'Other'
+        markup = select :province, options
+        markup.should have_tag('select') do |select|
+          select.should have_tag('optgroup[@label=Canada]') do |group|
+            group.should have_tag('option[@value=ab]', 'Alberta')
+            group.should have_tag('option[@value=bc]', 'British Columbia')
+          end
+          select.should have_tag('optgroup[@label="United States"]') do |group|
+            group.should have_tag('option[@value=oh]', 'Ohio')
+          end
+          select.should have_tag('option[@value=other]', 'Other')
+        end
+      end
     end
   end
 end
