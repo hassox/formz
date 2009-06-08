5d9238f4c49b034a7c967a356656e1e978a44344
visionmedia <tj@vision-media.ca>
2009-06-08 09:13:27 -0700

    Added label specs

---
+++ spec/labels_spec.rb
@@ -0,0 +1,16 @@
+
+describe Formz do
+  describe "labels" do
+    it "should add a label element" do
+      tag(:textarea, :name => 'comments', :label => 'Comments').
+      should have_tag('label[@for=comments]', 'Comments:')
+    end
+    
+    it "should allow an element to be required" do
+      tag(:textarea, :name => 'comments', :label => 'Comments', :required => true).
+      should have_tag('label[@for=comments]', 'Comments*:') do |label|
+        label.should have_tag('em', '*')
+      end
+    end
+  end
+end
