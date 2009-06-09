16522b9592c9f8a90c6d43e7b558c21292062c64
visionmedia <tj@vision-media.ca>
2009-06-09 09:39:46 -0700

    Reseting model

--- lib/formz/models.rb
+++ lib/formz/models.rb
@@ -8,11 +8,14 @@ module Formz
   module Models
     
     ##
-    # Return a form.
+    # Return a form in context to _model_.
     
     def form_for model, attrs = {}, &block
+      @__form_model = model
       name = model.class.to_s.split('::').last.downcase
       form name, attrs, &block
+    ensure
+      @__form_model = nil 
     end 
     
   end
