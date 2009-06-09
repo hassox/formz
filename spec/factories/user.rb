9b1c14b1ede74f03a7696038fb824b81b109455a
visionmedia <tj@vision-media.ca>
2009-06-09 09:04:56 -0700

    Fixed factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,15 +1,17 @@
 
 class User
   include DataMapper::Resource
-  property :id,    Serial
+  property :uid,   Serial
   property :name,  String, :length => 5..20
   property :email, String, :format => :email_address
   property :role,  String
+  property :signature, Text
 end
 
 Factory.define :user do |user|
-  user.id = 1
-  user.name = 'tjholowaychuk'
-  user.email = 'tj@vision-media.ca'
-  user.role = 'admin'
+  user.uid 1
+  user.name 'tjholowaychuk'
+  user.email 'tj@vision-media.ca'
+  user.role 'admin'
+  user.signature 'Foo bar'
 end
