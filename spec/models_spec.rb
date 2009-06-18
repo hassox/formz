638a04827e5d1044e06c2f8d4bcd32c483b50fec
visionmedia <tj@vision-media.ca>
2009-06-18 10:26:28 -0700

    More model specs

--- spec/models_spec.rb
+++ spec/models_spec.rb
@@ -7,12 +7,23 @@ describe Formz do
     
     describe "#form_for" do
       it "should create a form in context to a model, populating fields" do
-        form_for @user do
+        markup = form_for @user do
           hidden :id
           text :name
           text :email
           textarea :signature
         end
+        markup.should have_tag('input[@name=user[id]]') do |id|
+          name['value'].should == '1'
+        end
+        markup.should have_tag('input[@name=user[name]]') do |name|
+          name['value'].should == 'tjholowaychuk'
+        end
+        markup.should have_tag('input[@name=user[email]]') do |email|
+          name['value'].should == 'tj@vision-media.ca'
+        end
+        markup.should have_tag('textarea[@name=user[signature]]', 'Foo bar')
+        markup.should_not have_tag('input[@name=_method[user]]')
       end
       
       it "should override defaults when a value is present" do
